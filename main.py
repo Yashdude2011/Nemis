368538627371a9b85576160c849bf7d1d5b3405e
Yashdude2011 <111408953+Yashdude2011@users.noreply.github.com>
2022-08-17 11:46:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("made by nimit")
+import pyautogui
+pyautogui.FAILSAFE = False
+while True:
+        pyautogui.write('what is facial nerve')
+        pyautogui.press('enter')
+        pyautogui.press('ctrl')
+        pyautogui.write('https://youtu.be/4k4fXYVN8G4')
+        pyautogui.press('enter')
+        pyautogui._getCommaToken('stop')
+
+
